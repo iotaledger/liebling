74e1b6761e7f847f2cfa81e6e74df2cf684f9feb
Begoña Alvarez <balvarez@boxfish.studio>
2021-11-26 11:27:10 +0100

    add webassets footer

--- assets/js/fetchFooter.js
+++ assets/js/fetchFooter.js
@@ -1,46 +1,46 @@
-const FOUNDATION_DATA_URL = 'https://webassets.iota.org/data/foundation.json'
+const FOUNDATION_DATA_URL = 'https://webassets.iota.works/data/assembly.json'
 
 fetch(FOUNDATION_DATA_URL)
     .then((resp) => resp.json())
-    .then(function (data) {
+    .then(function(data) {
         if (data.information) {
-            let foundationLegalData = data.information
-            foundationLegalData.forEach(function (item) {
+            let assemblyLegalData = data.information
+            assemblyLegalData.forEach(function(item) {
                 let span = document.createElement('span')
                 span.innerHTML += '</br>' + item.label
-                document.getElementById('foundationLegal').appendChild(span)
+                document.getElementById('assemblyLegal').appendChild(span)
 
                 let links = item.urls
                 if (links) {
-                    links.forEach(function (item, index) {
+                    links.forEach(function(item, index) {
                         let anchor = document.createElement('a')
                         anchor.innerHTML = item.label
                         anchor.href = item.url
-                        document.getElementById('foundationLegal').appendChild(anchor)
+                        document.getElementById('assemblyLegal').appendChild(anchor)
 
                         if (links[index + 1]) {
                             let span = document.createElement('span')
                             span.innerHTML += ','
-                            document.getElementById('foundationLegal').appendChild(span)
+                            document.getElementById('assemblyLegal').appendChild(span)
                         }
                     })
                 }
                 if (item.value) {
                     let span = document.createElement('span')
                     span.innerHTML = ' ' + item.value
-                    document.getElementById('foundationLegal').appendChild(span)
+                    document.getElementById('assemblyLegal').appendChild(span)
                 }
             })
         }
         if (data.registeredAddress) {
             let registeredAddressData = data.registeredAddress.value
-            registeredAddressData.forEach(function (item) {
+            registeredAddressData.forEach(function(item) {
                 let span = document.createElement('span')
-                document.getElementById('foundationRegisteredAddress').appendChild(span)
+                document.getElementById('assemblyRegisteredAddress').appendChild(span)
                 span.innerHTML += item
             })
         }
     })
-    .catch(function (error) {
+    .catch(function(error) {
         /*   console.log(error)*/
     })
