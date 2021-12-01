e9990e903e3629ce2198e1bd26a8be4bf9db7027
Begoña Alvarez <balvarez@boxfish.studio>
2021-12-01 18:38:47 +0100

    Fetch fonts from webassets

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -41,7 +41,6 @@ mix.js('js/helpers.js', 'js/')
         processCssUrls: false
     })
     .copy('sass/fonts/icomoon/*.*', '../assets/fonts/icomoon/')
-    .copy('sass/fonts/carmen/*.*', '../assets/fonts/carmen/')
     .copy('assets/img/*.*', '../assets/img/')
     // .copyDirectory('sass/fonts/source-sans-pro/', '../assets/fonts/source-sans-pro/')
     .copy('js/vendor/content-api.min.js', '../assets/js/vendor/')
